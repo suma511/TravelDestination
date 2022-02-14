08d6958f2b447647b2c4b5ecc0e5efc511d697c9
suma511 <eliansumaia@gmail.com>
2022-02-15 00:44:57 +0200

    new

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,12 +3,11 @@ function Footer() {
     return (
 
         <>
-           <a href="https://dolnyslask.travel/?lang=en&gclid=CjwKCAiA9aKQBhBREiwAyGP5laqqtavK4QjiwTHsoxXnotebYVaVspU6kn-LSHCoZhteCSh7hkL9sBoCvhMQAvD_BwE">visitPoland</a>
-           <a href="https://www.ireland.com/en/">ireland</a>
-           <a href="https://www.ireland.com/en/">Rome</a>
-           <a href="https://www.ireland.com/en/">Vinna</a>
+           
 
-            <p class ='sum'>Written by Sumayya Elian</p>
+            <p>Written by Sumayya Elian</p>
+            <a href="https://web.facebook.com/profile.php?id=100070263295060">Contact me </a>
+
         </>
 
     );
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,16 +1,22 @@
+import './Home.css';
 import Tours from "../Tours/Tours";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Data from '../../data/Data.json';
+import TourDetails from '../TourDetails/TourDetails';
+
+
+
+
 function Home() {
     return (
 
         <>
             <Header />
+            
 
-            <Tours data = {Data}/>
-
-            <Footer/> 
+            <Tours data={Data} />
+            <Footer />
         </>
 
     );
